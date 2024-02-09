4f8e2978e04942ccbacace893a6a90d37c49869e
FFED246529C6BEDED02CAEAF <jameskady41@gmail.com>
2024-02-09 19:46:27 +0300

    dynamic routing

--- headlines.py
+++ headlines.py
@@ -11,7 +11,18 @@ feeds = {'complex': 'https://www.complex.com/index.xml',
          'bd':'https://www.businessdailyafrica.com/service/rss/bd/1939132/feed.rss',
          'cnw':'https://www.celebritynetworth.com/category/articles/feed/',
          'sky':'https://feeds.skynews.com/feeds/rss/technology.xml',
-         'cbs':'https://www.cbsnews.com/latest/rss/main'}
+         'cbs':'https://www.cbsnews.com/latest/rss/main',
+         'pulse':'https://www.pulselive.co.ke/rss',
+         'topstories':'https://abcnews.go.com/abcnews/topstories',
+         'usheadlines':'https://abcnews.go.com/abcnews/usheadlines',
+         'internationalheadlines':'https://abcnews.go.com/abcnews/internationalheadlines',
+         'politicsheadlines':'https://abcnews.go.com/abcnews/politicsheadlines',
+         'moneyheadline':'https://abcnews.go.com/abcnews/moneyheadlines',
+         'celebrity':'https://www.buzzfeed.com/celebrity.xml',
+         'tvandmovies':'https://www.buzzfeed.com/tvandmovies.xml',
+         'reader':'https://www.buzzfeed.com/reader.xml',
+         'spm':'https://spmbuzz.com/feed/',
+         }
 
 @app.route("/")
 
